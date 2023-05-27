45809a5354498d6730d75449fa2f14353c87e93d
antoine chilewski <chilewski.a@gmail.com>
2023-05-27 15:00:16 +0200

    hope it's work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,21 @@
+require 'open-uri'
 require 'json'
-require 'rest-client'
-
-puts "Cleaning database..."
-# Movie.destroy_all
-
-puts 'Seeding...'
-response = RestClient.get 'https://tmdb.lewagon.com/movie/top_rated'
-repos = JSON.parse(response)
-# => repos is an `Array` of `Hashes`.
-puts "Creating movies..."
-repos['results'].each do |h|
-  movie = Movie.new(
-    title: h['title'],
-    overview: h['overview'],
-    poster_url: "https://image.tmdb.org/t/p#{h['poster_path']}",
-    rating: h['vote_average']
-  )
-  movie.save!
-  puts "Created #{movie.title}"
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
-puts "Finished!"
+puts "Movies created"
